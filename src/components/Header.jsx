184979e30a492a6858e002cf2373b7b6f32ad4b6
chiboyL <chinonsoekezie3@gmail.com>
2025-05-25 00:31:17 -0900

    Added some emojis t the buttons and some descriptive text about the game

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,13 @@ function Header({score, bestScore}) {
   return(
     <>
         <h1 className="text-slate-200 text-4xl font-bold text-center mt-5">MEMORY CARD</h1>
-        <div className="score-board flex align-center justify-center gap-4 mt-5">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-white">Welcome to the My Game</h1>
+          <p className="text-white text-center p-5">Click the kitten cards and make sure you don't click the same cards twice 🙂 <br />GoodLuck 😉</p> 
+          <div className="score-board flex align-center justify-center gap-4 mt-5">   
             <p className="bg-slate-200 w-max p-3 rounded cursor-pointer text-lg">Current Score: {score}</p>
-            <p className="bg-slate-200 w-max p-3 rounded cursor-pointer text-lg">Highest Score: {bestScore}</p>
+            <p className="bg-slate-200 w-max p-3 rounded cursor-pointer text-lg">Highest Score: {bestScore}🚀</p>
+          </div>
         </div>
     </>
   )
